b6a68ebf74537d82f87acb8b904fcb4d00f5fa15
Edouard Poor <edouard@example.com>
2025-09-17 09:07:08 +1200

    Update project files

--- cc.sh
+++ cc.sh
@@ -5,7 +5,7 @@
 
 # Use Novita and Qwen Coder
 export ANTHROPIC_BASE_URL="https://api.novita.ai/anthropic"
-export ANTHROPIC_AUTH_TOKEN="sk_fDsXSKvtEtBbk2iomAQnx8HxDkzRyoNu_kOPusWvmBM"
+export ANTHROPIC_AUTH_TOKEN="<your key here>"
 export ANTHROPIC_MODEL="qwen/qwen3-coder-480b-a35b-instruct"
 export ANTHROPIC_SMALL_FAST_MODEL="qwen/qwen3-coder-480b-a35b-instruct"
 #export ANTHROPIC_MODEL="moonshotai/kimi-k2-instruct"
